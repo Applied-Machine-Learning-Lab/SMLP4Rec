72287e5eab8f3d77361f6911ce38ec7ead1cda30
Gaojingtong <96113694+Gaojingtong@users.noreply.github.com>
2024-02-27 12:25:51 +0800

    Update smlprec.py

--- smlprec.py
+++ smlprec.py
@@ -4,7 +4,22 @@ from functools import partial
 from recbole.model.abstract_recommender import SequentialRecommender
 from recbole.model.layers import TransformerEncoder, FeatureSeqEmbLayer, VanillaAttention
 from recbole.model.loss import BPRLoss
-from recbole.ext_lib.model.mlp.sMLP_block import sMLPBlock
+
+
+class sMLPBlock(nn.Module):
+    def __init__(self,h,w,c):
+        super().__init__()
+        self.proj_h=nn.Linear(h,h)
+        self.proj_w=nn.Linear(w,w)
+        self.fuse=nn.Linear(3*c,c)
+    
+    def forward(self,x):
+        x_h=self.proj_h(x.permute(0,1,3,2)).permute(0,1,3,2)
+        x_w=self.proj_w(x)
+        x_id=x
+        x_fuse=torch.cat([x_h,x_w,x_id],dim=1)
+        out=self.fuse(x_fuse.permute(0,2,3,1)).permute(0,3,1,2)
+        return out
 
 
 class TokenMixing(nn.Module):
